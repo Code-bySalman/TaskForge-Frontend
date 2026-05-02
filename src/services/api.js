d30b4a8d7317cf146f0e0b68a0053df51fb47092
Code-bySalman <usmanis@student.iul.ac.in>
2026-05-03 00:21:50 +0530

    fixed deployement issues

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+
+console.log("ENV VALUE:", import.meta.env.VITE_API_URL);
+console.log("FINAL API URL:", API_URL);
 
 const api = axios.create({
   baseURL: API_URL,
